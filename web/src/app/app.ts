3263cffa98ec6634176629af107fc8b3225983bf
Mitja Kurath <mitja.kurath@kyberna.com>
2026-05-04 12:35:33 +0200

    Added simplistic start page with spartan-ng components

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmCardImports } from '@spartan-ng/helm/card';
+import { HlmTypographyImports } from '@spartan-ng/helm/typography';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HlmButtonImports, HlmTypographyImports, HlmCardImports],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('web');
+  protected readonly title = signal('Koda');
+  protected readonly version = signal('0.0.1');
 }
